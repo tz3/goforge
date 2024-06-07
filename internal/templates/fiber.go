050365d21787a3687a445c94cd8aa65554fabf80
Moutaz Chaara <31438677+tz3@users.noreply.github.com>
2024-06-07 21:35:09 +0200

    Refactor the internal/template folder and add a proper documentation (#11)
    In this PR:-
    
    - Introduce a proper structure for the tempalte folder.
    - Change file names for routes.
    - Add proper documentation for the functions inside template folder.

---
+++ internal/templates/fiber.go
@@ -0,0 +1,88 @@
+// Package template provides a set of templates for the Fiber router.
+package template
+
+// FiberTemplate is a struct that provides methods to generate templates for a Fiber-based HTTP server.
+type FiberTemplate struct{}
+
+// Main returns the main template for the Fiber-based HTTP server.
+func (c FiberTemplate) Main() []byte {
+	return MakeFiberMain()
+}
+
+// Server returns the server template for the Fiber-based HTTP server.
+func (c FiberTemplate) Server() []byte {
+	return MakeFiberServer()
+}
+
+// Routes returns the routes template for the Fiber-based HTTP server.
+func (c FiberTemplate) Routes() []byte {
+	return MakeFiberRoutes()
+}
+
+// MakeFiberServer returns a byte slice containing a Go source code template for a Fiber server.
+// The server is created with a new Fiber app.
+func MakeFiberServer() []byte {
+	return []byte(`package server
+
+import "github.com/gofiber/fiber/v2"
+
+type FiberServer struct {
+    *fiber.App
+}
+
+func New() *FiberServer {
+    server := &FiberServer{
+        App: fiber.New(),
+    }
+
+    return server
+}
+
+`)
+}
+
+// MakeFiberRoutes returns a byte slice containing a Go source code template for Fiber routes.
+// The template includes a function to register a hello world handler to the root path.
+func MakeFiberRoutes() []byte {
+	return []byte(`package server
+
+import (
+    "github.com/gofiber/fiber/v2"
+)
+
+func (s *FiberServer) RegisterFiberRoutes() {
+    s.App.Get("/", s.helloWorldHandler)
+}
+
+func (s *FiberServer) helloWorldHandler(c *fiber.Ctx) error {
+    resp := map[string]string{
+        "message": "Hello World",
+    }
+
+    return c.JSON(resp)
+}
+`)
+}
+
+// MakeFiberMain returns a byte slice containing a Go source code template for the main function.
+// The main function creates a new Fiber server, registers the routes, and starts the server.
+func MakeFiberMain() []byte {
+	return []byte(`package main
+
+import (
+    "{{.ProjectName}}/internal/server"
+)
+
+func main() {
+
+    server := server.New()
+
+    server.RegisterFiberRoutes()
+
+    err := server.Listen(":8080")
+    if err != nil {
+        panic("cannot start server")
+    }
+}
+`)
+}
---
+++ internal/templates/standard.go
@@ -0,0 +1,56 @@
+// Package template provides a set of templates for the standard library HTTP server.
+package template
+
+// StandardLibraryTemplate is a struct that provides methods to generate templates for a standard library-based HTTP server.
+type StandardLibraryTemplate struct{}
+
+// Main returns the main template for the standard library-based HTTP server.
+func (c StandardLibraryTemplate) Main() []byte {
+	return MainTemplate()
+}
+
+// Server returns the server template for the standard library-based HTTP server.
+func (c StandardLibraryTemplate) Server() []byte {
+	return MakeHTTPServer()
+}
+
+// Routes returns the routes template for the standard library-based HTTP server.
+func (c StandardLibraryTemplate) Routes() []byte {
+	return MakeHTTPRoutes()
+}
+
+// MakeHTTPRoutes returns a byte slice containing a Go source code template for standard library HTTP routes.
+// The template includes a function to register a hello world handler to the root path.
+func MakeHTTPRoutes() []byte {
+	return []byte(`package server
+
+import (
+    "net/http"
+    "encoding/json"
+    "log"
+)
+
+// RegisterRoutes creates a new standard library HTTP ServeMux, registers a hello world handler to the root path,
+// and returns the ServeMux.
+func (s *Server) RegisterRoutes() http.Handler {
+
+    mux := http.NewServeMux()
+    mux.HandleFunc("/", s.handler)
+
+    return mux
+}
+
+// handler is an HTTP handler that responds with a JSON containing a hello world message.
+func (s *Server) handler(w http.ResponseWriter, r *http.Request) {
+    resp := make(map[string]string)
+    resp["message"] = "Hello World"
+
+    jsonResp, err := json.Marshal(resp)
+    if err != nil {
+        log.Fatalf("error handling JSON marshal. Err: %v", err)
+    }
+
+    w.Write(jsonResp)
+}
+`)
+}
