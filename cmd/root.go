0bd542db406dda1f98d46219e7117367da651315
Moutaz <xmoutaz@hotmail.com>
2024-06-16 21:02:50 +0100

    Add application versioning

--- cmd/root.go
+++ cmd/root.go
@@ -7,23 +7,21 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
+// rootCmd is the main command for the application. It's invoked when no subcommands are specified.
 var rootCmd = &cobra.Command{
 	Use:   "goforge",
 	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Long: `A comprehensive description of your application, including examples and usage instructions. For instance:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
+	// Uncomment the following line to associate an action with the root command:
 	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
+// Execute is the entry point for the CLI. It adds all child commands to the root command and sets flags as needed.
+// It's invoked by the main function and should only be called once.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -31,14 +29,13 @@ func Execute() {
 	}
 }
 
+// init sets up flags and configuration settings for the application.
+// It's automatically called before the main function.
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
+	// Define global flags for your application here. Cobra supports persistent flags that are defined here.
 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.goforge.yaml)")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
+	rootCmd.AddCommand(versionCommand)
+	// Define local flags that are only valid for this command.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
